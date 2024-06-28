37ebfeb8c079b03699c2ee04e2f6500f9b85082f
binayakbhandari <binayakbhandari60@gmail.com>
2024-06-28 20:46:25 +0545

    add createblog

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import Form from './components/Form'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { baseUrl } from '../../config'
 
 const Login = () => {
   const navigate = useNavigate()
   const handleLogin = async (data) => {
     try {
-      const response = await axios.post("https://react30.onrender.com/api/user/login", data)
+      const response = await axios.post(`${baseUrl}/login`,data)
       if (response.status = 200) {
-        navigate('/login')
+        localStorage.setItem('token',response.data.token)
+        navigate('/')
       }
       else {
         alert("Login failed")
--- src/pages/blog/components/form/Form.jsx
+++ src/pages/blog/components/form/Form.jsx
@@ -1,38 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = ({type}) => {
-  return (
-    <div className="flex justify-center items-center w-screen h-screen bg-white">
-	
-	<div className="container mx-auto my-4 px-4 lg:px-20">
+const Form = ({ type, onSubmit }) => {
+	const [data, setData] = useState({
+		title: '',
+		subtitle: '',
+		image: '',
+		category : '',
+		description : ''
+	})
+	const handleChange = (e)=>{
+		const {name,value} = e.target 
+		setData({
+			...data,
+			[name] : name === 'image' ? e.target.files[0] : value
+		})
+	}
+	const handleSubmit = (e)=>{
+		e.preventDefault()
+		onSubmit(data)
+	}
+	return (
+		<div className="flex justify-center items-center w-screen h-screen bg-white">
 
-		<div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
-			<div className="flex">
-				<h1 className="font-bold uppercase text-5xl">{type} <br /> Blog</h1>
-			</div>
-			<div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
-				<input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-            type="text" placeholder="Title*" />
-				<input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-            type="text" placeholder="Subtitle*" />
-				<input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-            type="file" />
-				<input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-            type="text" placeholder="Category*" />
-        </div>
-				<div className="my-4">
-					<textarea placeholder="Description*" className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
-				</div>
-				<div className="my-2 w-1/2 lg:w-1/4">
-					<button className="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
+			<div className="container mx-auto my-4 px-4 lg:px-20">
+
+				<div className="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
+					<div className="flex">
+						<h1 className="font-bold uppercase text-5xl">{type} <br /> Blog</h1>
+					</div>
+					<form onSubmit={handleSubmit}>
+					<div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
+						<input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+							type="text" placeholder="Title*" name='title' onChange={handleChange} />
+						<input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+							type="text" placeholder="Subtitle*" name='subtitle' onChange={handleChange} />
+						<input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+							type="file" name='image' onChange={handleChange} />
+						<input className="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+							type="text" placeholder="Category*" name='category' onChange={handleChange} />
+					</div>
+					<div className="my-4">
+						<textarea placeholder="Description*" onChange={handleChange} name='description' className="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+					</div>
+					<div className="my-2 w-1/2 lg:w-1/4">
+						<button className="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
                       focus:outline-none focus:shadow-outline">
-            Submit
-          </button>
+							Submit
+						</button>
+					</div>
+					</form>
 				</div>
 			</div>
-    </div>
-</div>
-  )
+		</div>
+	)
 }
 
 export default Form
