8643499e4bb3c8fc71c71b9c66098f4d8757d657
binayakbhandari <binayakbhandari60@gmail.com>
2024-06-29 15:04:57 +0545

    implement all blog

--- src/pages/blog/components/card/Card.jsx
+++ src/pages/blog/components/card/Card.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Card = () => {
+const Card = ({blog}) => {
   return (
-    <Link to="blog/id">
+  <Link to={`/blog/${blog._id}`}>
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="https://t4.ftcdn.net/jpg/00/91/12/55/360_F_91125583_TDpl4eZx3joQHHAZhyfK5aPBkj5l3JsM.jpg" alt="Sunset in the mountains" />
+      <img className="w-full" src={blog.imageUrl} alt="Sunset in the mountains" />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <div className="font-bold text-xl mb-2">{blog.title}</div>
         <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          {blog.description}
         </p>
       </div>
       <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{blog.category}</span>
       </div>
     </div>
     </Link>
